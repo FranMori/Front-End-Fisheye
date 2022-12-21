49c6dacbe75e4eb9200ffd03f1c203985eda210c
FranMori <francois.moriame@gmail.com>
2022-12-21 11:42:04 +0100

    Etape 2 : Importer les datas completed

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,37 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, id, tagline, city, country, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+        article.setAttribute("id", id)
+
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
+        const h3TagLine = document.createElement('h3');
+        h3TagLine.setAttribute("class", 'tagLine')
+        h3TagLine.textContent = tagline
+
+        const h3Location = document.createElement('h3');
+        h3Location.setAttribute("class", "country")
+        h3Location.textContent = city + ', ' + country
+
+        const h3Price = document.createElement('h3')
+        h3Price.setAttribute("class", 'price')
+        h3Price.textContent = price+'€/jour'
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3Location);
+        article.appendChild(h3TagLine);
+        article.appendChild(h3Price);
+
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, tagline, city, country, price, getUserCardDOM }
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,31 +1,24 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
+// Récupération de l'url du JSON et fetch des datas
+let url = "./data/photographers.json"
+
+function getData() {
+    return fetch(url)
+    .then((res) => res.json())
+
+}
+
+// Function pour récupérer les datas photographers
+async function getPhotographers() {
+        let datas = await getData()
+        console.log(datas)
+
+        let photographers = datas.photographers
+
         return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+            photographers: [...photographers]})
     }
 
+// Function pour display les datas en html
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
 
@@ -35,9 +28,9 @@
             photographersSection.appendChild(userCardDOM);
         });
     };
-
+    
+// Function pour initier le tout
     async function init() {
-        // Récupère les datas des photographes
         const { photographers } = await getPhotographers();
         displayData(photographers);
     };
