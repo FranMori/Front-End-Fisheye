a7f3d18f81c6059abccd505c915f2f7077fcd4b0
FranMori <francois.moriame@gmail.com>
2022-12-29 11:22:29 +0100

    Navigation entre accueil et page photographe

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -4,11 +4,15 @@ function photographerFactory(data) {
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
+        const link = document.createElement('a')
+        link.setAttribute("href", "../../photographer.html?" + id)
+
         const article = document.createElement( 'article' );
         article.setAttribute("id", id)
 
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        img.setAttribute("Alt", "Photo de profil du photographe")
 
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
@@ -25,13 +29,14 @@ function photographerFactory(data) {
         h3Price.setAttribute("class", 'price')
         h3Price.textContent = price+'€/jour'
 
+        link.appendChild(article)
         article.appendChild(img);
         article.appendChild(h2);
         article.appendChild(h3Location);
         article.appendChild(h3TagLine);
         article.appendChild(h3Price);
 
-        return (article);
+        return (link);
     }
-    return { name, picture, tagline, city, country, price, getUserCardDOM }
+    return { name, picture, tagline, city, country, price,  getUserCardDOM }
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,10 @@
-//Mettre le code JavaScript lié à la page photographer.html
+//Récupération de l'id dans l'url
+
+const url = window.location.search
+const justId = url.slice(1)
+
+
+// Fetch des données
+
+
+
